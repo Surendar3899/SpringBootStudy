6a97af5a0a57e9a603c73a5340f447310cb19abd
Surendar N <surendar3899@gmail.com>
2024-11-23 20:23:23 +0530

    changesss

--- myWebsite/src/main/java/com/payilagam/IndexController.java
+++ myWebsite/src/main/java/com/payilagam/IndexController.java
@@ -3,23 +3,29 @@ package com.payilagam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 
 @Controller
 public class IndexController {
 	
 	@RequestMapping("index")
-	public String index() {
+	public String index(HttpServletRequest request) {
 		System.out.println("index called");
-		return "demo.jsp";
+		HttpSession session = request.getSession();
+		String name = request.getParameter("name");
+		session.setAttribute("usern", name);
+		return "demo";
 		
 	}
 	
 	
-	@RequestMapping("/demo")
-	public String demo() {
-		System.out.println("demo called");
-		return "index.jsp";
-		
-	}
+//	@RequestMapping("demo")
+//	public String demo() {
+//		System.out.println("demo called");
+//		return "index.jsp";
+//		
+//	}
 
 }
--- myWebsite/src/main/webapp/admin/demo.jsp
+++ myWebsite/src/main/webapp/admin/demo.jsp
@@ -4,6 +4,8 @@
 
 <body>
 <h1>This is my demo jsp</h1>
+
+${usern}
 </body>
 
 </html>
